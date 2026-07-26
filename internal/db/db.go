57d15ec36c73d192fde376fc62722c0c4cea8dce
orionvega2343-cloud <orionvega2343@gmail.com>
2026-07-27 00:07:06 +0300

    feat: add connect database

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"NorthlaneVPN/internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func Connect(cfg config.DB) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf("name: %s, host, %s, port: %d, username: %s, password: %s, ssl_mode: %s", cfg.Name, cfg.Host, cfg.Port, cfg.User, cfg.Password)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
