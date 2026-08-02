b6d6751109a7fe2a78a3afbab64598163e2851c3
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-02 15:12:35 +0300

    feat: add migration for subscription_servers junction table

---
+++ migrations/0007_create_subscription_servers_table.up.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS junction_table(
+    -- Соединение первичных ключей таблицы подписок и серверов
+    subscription_id INT NOT NULL,
+    server_id INT NOT NULL,
+    -- создание составного первичного ключа
+    PRIMARY KEY(subscription_id, server_id),
+    -- связывание с таблицей подписок
+    CONSTRAINT fk_subscriptions
+        FOREIGN KEY (subscription_id)
+        REFERENCES subscriptions(id)
+        ON DELETE CASCADE,
+
+    -- связывание с таблицей серверов
+    CONSTRAINT fk_servers
+        FOREIGN KEY (server_id)
+        REFERENCES servers(id)
+        ON DELETE CASCADE
+)
