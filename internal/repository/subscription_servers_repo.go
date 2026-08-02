fe46cdba4796ce129437482cdaca785f49fe9ee2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-08-02 15:48:14 +0300

    feat: add subscriptio_servers repo for many-to-many one subscription for merging servers

---
+++ internal/repository/subscription_servers_repo.go
@@ -0,0 +1,28 @@
+package repository
+
+import (
+	"NorthlaneVPN/internal/models"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type SubscriptionServersRepo interface {
+	GetServersForSubscription(subscriptionId int) ([]models.Server, error)
+}
+
+type SubscriptionServersRepoImpl struct {
+	db *sqlx.DB
+}
+
+func NewSubscriptionServersRepo(db *sqlx.DB) *SubscriptionServersRepoImpl {
+	return &SubscriptionServersRepoImpl{db: db}
+}
+
+func (r *SubscriptionServersRepoImpl) GetServersForSubscription(subscriptionId int) ([]models.Server, error) {
+	var servers []models.Server
+	err := r.db.Select(&servers, `SELECT  id, host, port, protocol FROM servers JOIN subscription_servers ON servers.id = subscription_servers.server_id WHERE subscription_servers.subscription_id = $1`, subscriptionId)
+	if err != nil {
+		return nil, err
+	}
+	return servers, nil
+}
